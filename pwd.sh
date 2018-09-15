786d6a1ddaed612ca75f2f94e988f606e66992b2
Ronskerz415 <ronald.latney@hgs.hiddengeniusproject.org>
2018-09-15 13:33:33 -0700

    done

---
+++ pwd.sh
@@ -0,0 +1,24 @@
+pwd					# shows you the file path
+cd ~/Desktop					# Takes you to the desktop folder
+mkdir website-filesystem					#creates new folder called website_filesystems
+cd website-filesystem					# changes your directory to website_filesystems
+touch index.html					# creates index.html file
+mkdir html					#creates html folder
+mkdir css					#creates css folder
+mkdir js					#creates js folder
+mkdir img					#create images folder
+mkdir extras					#create extras folder
+cd html					# takes you to html folder
+touch pg1.html					#creates file pg1.html in html folder
+touch pg2.html					#creates file pg2.html in html folder
+cd ../css					#changes your directory to the css folder
+touch pg1.css					#creates pg1.css file
+touch pg2.css					#creates pg2.css file
+touch index.css
+cd ../js					#changes directory to the js folder
+touch pg1.js					#creates pg1.js file
+touch pg2.js					#creates pg2.js file
+touch index.js					#creates index.js file
+cd ../extras				#changes directory to extras folder
+mkdir audio					#makes audio folder
+mkdir video					#makes video folder
